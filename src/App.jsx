6800e2dc389d6b6c9271e7b11fdca8ee793223b4
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2026-04-03 09:09:59 +0200

    feat: initial project setup with react and tailwind css v4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+export default function App() {
+  return (
+    <>
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </>
+  );
+};
