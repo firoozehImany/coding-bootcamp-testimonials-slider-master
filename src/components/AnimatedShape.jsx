57378aa00d0c850ccb26b5016fb98bd79926a6f8
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2026-04-13 23:26:10 +0200

    refactor: improve animated shapes logic and animation

--- src/components/AnimatedShape.jsx
+++ src/components/AnimatedShape.jsx
@@ -1,19 +1,14 @@
 export default function AnimatedShape() {
   return (
-<>
-    <div 
-      className="absolute top-[60px] right-80 w-[460px] h-[320px] bg-light-purple animate-fluid -rotate-[10deg]"
-      style={{ 
-        borderRadius: "69% 31% 41% 59% / 76% 50% 30% 24%"  
-      }}
-    />
-        <div 
-      className="absolute bottom-[60px] right-40 w-[360px] h-[600px] bg-dark-purple animate-fluid -rotate-[40deg]"
-      style={{ 
-        borderRadius: "69% 31% 41% 59% / 76% 50% 30% 24%" 
-      }}
-    />
-</>
-  
+    <div className="absolute inset-0 pointer-events-none">
+      <div
+        className="absolute top-10 right-60 w-[460px] h-[320px] bg-light-purple animate-fluid -rotate-[10deg]"
+        style={{ borderRadius: "69% 31% 41% 59% / 76% 50% 30% 24%" }}
+      />
+      <div
+        className="absolute -bottom-10 right-20 w-[360px] h-[600px] bg-dark-purple animate-fluid -rotate-[40deg]"
+        style={{ borderRadius: "69% 31% 41% 59% / 76% 50% 30% 24%" }}
+      />
+    </div>
   );
 }
