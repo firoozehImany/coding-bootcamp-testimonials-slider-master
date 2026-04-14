a848587b87aad11256d637798bb296a78f849025
Firoozeh Imany <107725515+Firoozehimany@users.noreply.github.com>
2026-04-15 00:15:35 +0200

    chore: enable existing fluid background animations

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,23 +2,32 @@ import AnimatedShape from "./AnimatedShape";
 
 export default function Background() {
   return (
-    <div className="absolute inset-0 pointer-events-none -z-10 overflow-hidden">
-      {/* <AnimatedShape /> */}
-      <img
-        src="/images/pattern-quotes.svg"
-        alt=""
-        className="absolute top-[216px] left-64"
-      />
-      <img
-        src="/images/pattern-bg.svg"
-        alt=""
-        className="absolute top-[50px] right-18"
-      />
+    <div className="fixed inset-0 w-full h-screen pointer-events-none -z-10 overflow-hidden px-6">
+      
       <img
         src="/images/pattern-curve.svg"
         alt=""
-        className="absolute bottom-0 left-0"
+        className="absolute bottom-0 left-0 w-auto h-auto"
       />
+      
+      <div className="relative mx-auto w-full max-w-[1280px] h-full flex flex-col-reverse items-center justify-end gap-14 md:pt-0 pt-20 md:block">
+        <div className="relative md:absolute md:top-1/2 md:-translate-y-[150%] md:left-10">
+          <img
+            src="/images/pattern-quotes.svg"
+            alt=""
+            className="md:w-fit md:h-fit w-16 md:px-40"
+          />
+        </div>
+        <div className="relative flex justify-center items-center md:absolute md:top-1/2 md:-translate-y-1/2 md:right-0">
+          <AnimatedShape />
+          <img
+            src="/images/pattern-bg.svg"
+            alt=""
+            className="relative z-0 w-fit h-fit"
+          />
+        </div>
+        
+      </div>
     </div>
   );
 }
